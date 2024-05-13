5b3742b339bc8a6280d8734dba2932dd07bfbd58
hyunw9 <rkdgusdnr32@naver.com>
2024-05-13 22:47:25 +0900

    [chore/#2] : 클래스명 수정

--- src/main/java/sopt/seminar/sopt/common/dto/ErrorResponse.java
+++ src/main/java/sopt/seminar/sopt/common/dto/ErrorResponse.java
@@ -1,17 +1,18 @@
 package sopt.seminar.sopt.common.dto;
 
-import sopt.seminar.sopt.common.dto.response.statusEnum.ErrorStatus;
+import sopt.seminar.sopt.common.exception.errorStatusEnum.ErrorMessage;
 
 public record ErrorResponse(
-      int status,
-      String message
+    int status,
+    String message
 
-  ) {
-    public static ErrorResponse of(int status, String message) {
-      return new ErrorResponse(status, message);
-    }
+) {
 
-    public static ErrorResponse of(ErrorStatus errorStatus) {
-      return new ErrorResponse(errorStatus.getStatus(), errorStatus.getMessage());
-    }
+  public static ErrorResponse of(int status, String message) {
+    return new ErrorResponse(status, message);
+  }
+
+  public static ErrorResponse of(ErrorMessage errorMessage) {
+    return new ErrorResponse(errorMessage.getStatus(), errorMessage.getMessage());
+  }
 }
