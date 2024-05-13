b85c146990705fdc53f847558f332883f4c854fa
hyunw9 <rkdgusdnr32@naver.com>
2024-05-13 22:46:17 +0900

    [feat/#2] : Error Response 추가

--- src/main/java/sopt/seminar/sopt/common/exception/errorStatusEnum/ErrorMessage.java
+++ src/main/java/sopt/seminar/sopt/common/exception/errorStatusEnum/ErrorMessage.java
@@ -1,11 +1,11 @@
-package sopt.seminar.sopt.common.dto.response.statusEnum;
+package sopt.seminar.sopt.common.exception.errorStatusEnum;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public enum ErrorStatus {
+public enum ErrorMessage {
   ;
   private final int status;
   private final String message;
