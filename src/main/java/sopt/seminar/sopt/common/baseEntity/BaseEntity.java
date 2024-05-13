d75fb5608fc33cf195a568c55ca3d765757bef9f
hyunw9 <rkdgusdnr32@naver.com>
2024-05-13 19:51:34 +0900

    [feat/#2] : 베이스 엔티티 및 Configuration 추가

---
+++ src/main/java/sopt/seminar/sopt/common/baseEntity/BaseEntity.java
@@ -0,0 +1,23 @@
+package sopt.seminar.sopt.common.baseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Entity
+@EnableJpaAuditing
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity {
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+}
