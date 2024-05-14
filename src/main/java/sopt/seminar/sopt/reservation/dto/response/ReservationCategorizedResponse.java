cd84918d05747e648cde6d187f3242126ebbc5cf
hyunw9 <rkdgusdnr32@naver.com>
2024-05-14 21:59:07 +0900

    [docs/#5] : null 인 값은 Json Serialize 무시하도록 설정

--- src/main/java/sopt/seminar/sopt/reservation/dto/response/ReservationCategorizedResponse.java
+++ src/main/java/sopt/seminar/sopt/reservation/dto/response/ReservationCategorizedResponse.java
@@ -1,8 +1,10 @@
 package sopt.seminar.sopt.reservation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ReservationCategorizedResponse(
     Long reservationId,
     String userName,
