1c52232135f4e95aa28cc1485f7ecd7bd42ef04b
hyunw9 <rkdgusdnr32@naver.com>
2024-05-14 21:56:54 +0900

    [chore/#5] : ErrorMessage Enum 추가

--- src/main/java/sopt/seminar/sopt/common/exception/errorStatusEnum/ErrorMessage.java
+++ src/main/java/sopt/seminar/sopt/common/exception/errorStatusEnum/ErrorMessage.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorMessage {
   MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "멤버가 존재하지 않습니다."),
-
+  STORE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "가게가 존재하지 않습니다."),
   ;
   private final int status;
   private final String message;
