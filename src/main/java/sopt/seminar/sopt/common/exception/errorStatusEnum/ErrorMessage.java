1cbf50db1179c7aa5bf66d11abb9706e92f7242c
hyunw9 <rkdgusdnr32@naver.com>
2024-05-14 20:33:38 +0900

    [feat/#5] : 커스텀 예외 처리 구현

--- src/main/java/sopt/seminar/sopt/common/exception/errorStatusEnum/ErrorMessage.java
+++ src/main/java/sopt/seminar/sopt/common/exception/errorStatusEnum/ErrorMessage.java
@@ -2,10 +2,13 @@ package sopt.seminar.sopt.common.exception.errorStatusEnum;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Getter
 public enum ErrorMessage {
+  MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "멤버가 존재하지 않습니다."),
+
   ;
   private final int status;
   private final String message;
