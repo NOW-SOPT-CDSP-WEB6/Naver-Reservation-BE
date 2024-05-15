5f532d2885535e2327c01a3bf7aa33e3ecc5272b
softmoca <acyc0909@gmail.com>
2024-05-15 11:07:12 +0900

    [feat/#3]: API4 리뷰 작성 베이스

---
+++ src/main/java/sopt/seminar/sopt/reservation/service/PostReviewDTO.java
@@ -0,0 +1,14 @@
+package sopt.seminar.sopt.reservation.service;
+
+public class PostReviewDTO {
+    private String content;
+
+    // Getter 및 Setter 메서드
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/sopt/seminar/sopt/reservation/service/ReservationPostReviewService.java
@@ -0,0 +1,34 @@
+package sopt.seminar.sopt.reservation.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sopt.seminar.sopt.reservation.entity.Reservation;
+import sopt.seminar.sopt.reservation.repository.ReservationJpaRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationPostReviewService {
+    private final ReservationJpaRepository reservationJpaRepository;
+
+    public boolean postReview(Long reservationId, PostReviewDTO postReviewRequest) {
+    Optional<Reservation> optionalReservation = reservationJpaRepository.findById(reservationId);
+
+    // 예약이 존재하지 않으면 처리 중단
+        if (optionalReservation.isEmpty()) {
+        return false;
+    }
+
+    Reservation reservation = optionalReservation.get();
+    // 요청에서 받은 데이터를 Reservation 엔티티의 content 속성에 저장
+        reservation.setContent(postReviewRequest.getContent());
+
+    // 변경된 예약 정보를 저장
+        reservationJpaRepository.save(reservation);
+
+        return true;
+}
+
+}
