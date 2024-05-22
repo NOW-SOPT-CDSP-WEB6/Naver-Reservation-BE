a4c5320e7fe6b1abef9b630b20d889c2f85ccb9d
hyunw9 <rkdgusdnr32@naver.com>
2024-05-22 18:26:45 +0900

    [refactor/#9] : 서비스 로직 변경

--- src/main/java/sopt/seminar/sopt/reservation/service/ReservationCountPriceService.java
+++ src/main/java/sopt/seminar/sopt/reservation/service/ReservationCountPriceService.java
@@ -4,7 +4,10 @@ package sopt.seminar.sopt.reservation.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+import sopt.seminar.sopt.common.exception.NotFoundException;
+import sopt.seminar.sopt.common.exception.errorStatusEnum.ErrorMessage;
 import sopt.seminar.sopt.member.entity.Member;
+import sopt.seminar.sopt.reservation.entity.Category;
 import sopt.seminar.sopt.reservation.entity.Reservation;
 import sopt.seminar.sopt.reservation.repository.ReservationJpaRepository;
 
@@ -16,31 +19,44 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ReservationCountPriceService {
 
-    private final ReservationJpaRepository reservationJpaRepository;
+  private final ReservationJpaRepository reservationJpaRepository;
 
-    public Map<String, Integer> getCountPriceByCategory(Long memberId, String category) {
-        List<Reservation> reservations;
+  public Map<String, Integer> getCountPriceByCategory(Long memberId, String category) {
+    List<Reservation> reservations;
 
-        if (category != null && !category.isEmpty()) {
-            // category 파라미터가 제공되었을 경우 해당 카테고리의 예약만 가져옴
-            reservations = reservationJpaRepository.findByMemberIdAndCategory(memberId, category);
-        } else {
-            // category 파라미터가 없을 경우 memberId에 해당하는 모든 예약을 가져옴
-            Member member = new Member(memberId);
-            reservations = reservationJpaRepository.findByMember(member);
+    if (category == null) {
+      reservations = findEveryMemberReservation(memberId);
+    } else {
 
-        }
+      Category inputCategory;
 
-        int count = reservations.size();
-        int total = reservations.stream()
-                .mapToInt(reservation -> reservation.getPrice() != null ? reservation.getPrice() : 0)
-                .sum();
+      try {
+        inputCategory = Category.valueOf(category);
+      } catch (Exception e) {
+        throw new NotFoundException(ErrorMessage.CATEGORY_NOT_FOUND);
+      }
 
-        Map<String, Integer> result = new HashMap<>();
-        result.put("count", count);
-        result.put("totalPrice", total);
-
-        return result;
+      reservations = findReservationGroupByCategory(memberId, inputCategory);
     }
+
+    int count = reservations.size();
+    int total = reservations.stream()
+        .mapToInt(reservation -> reservation.getPrice() != null ? reservation.getPrice() : 0)
+        .sum();
+
+    Map<String, Integer> result = new HashMap<>();
+    result.put("count", count);
+    result.put("totalPrice", total);
+
+    return result;
+  }
+
+  private List<Reservation> findReservationGroupByCategory(Long memberId, Category inputCategory) {
+    return reservationJpaRepository.findByMemberIdAndCategory(memberId, inputCategory);
+  }
+
+  private List<Reservation> findEveryMemberReservation(Long memberId) {
+    return reservationJpaRepository.findByMemberId(memberId);
+  }
 
 }
