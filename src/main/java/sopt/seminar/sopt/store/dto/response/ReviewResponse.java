4c71f3ef1dfa0ead294ce766cce3a378be0f5bbc
hyunw9 <rkdgusdnr32@naver.com>
2024-05-23 18:34:04 +0900

    [refactor/#9] : 리뷰 반환 시 리뷰 ID 추가

--- src/main/java/sopt/seminar/sopt/store/dto/response/ReviewResponse.java
+++ src/main/java/sopt/seminar/sopt/store/dto/response/ReviewResponse.java
@@ -3,12 +3,13 @@ package sopt.seminar.sopt.store.dto.response;
 import java.time.LocalDateTime;
 
 public record ReviewResponse(
+    Long reviewId,
     String name,
     LocalDateTime reviewDate,
     String content
 ) {
 
-  public static ReviewResponse of(String name, LocalDateTime reviewDate, String content) {
-    return new ReviewResponse(name, reviewDate, content);
+  public static ReviewResponse of(Long reviewId, String name, LocalDateTime reviewDate, String content) {
+    return new ReviewResponse(reviewId, name, reviewDate, content);
   }
 }
